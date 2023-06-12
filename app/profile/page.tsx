d27ff031c2e3505af55eb558cb92fc125beacb5b
DongEun Kim <kdedong12@gmail.com>
2023-06-12 15:49:00 +0900

    init project

---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+import second from 'React'
+
+export default function Profile() {
+  return (
+    <h1>asdd</h1>
+  )
+}
---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+import second from 'React'
+
+export default function Resume() {
+  return (
+    <h1>asdd</h1>
+  )
+}
